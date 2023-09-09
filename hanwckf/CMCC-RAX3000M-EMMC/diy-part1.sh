e649a8f5f6d692630206a2da94d1aafb48deba84
RavenCage <126802578+RavenCage@users.noreply.github.com>
2023-09-09 15:02:14 +0800

    Update diy-part1.sh

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -15,12 +15,12 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
-echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall2.git;packages' >>feeds.conf.default
-echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git;luci' >>feeds.conf.default
+#echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall2.git;packages' >>feeds.conf.default
+#echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git;luci' >>feeds.conf.default
 echo 'src-git adguardhome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
