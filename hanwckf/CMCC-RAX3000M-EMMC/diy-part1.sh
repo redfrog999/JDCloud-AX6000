092b4a9a9e18015a6dfe17bdc079879ad94aaa6d
RavenCage <126802578+RavenCage@users.noreply.github.com>
2023-09-29 21:08:08 +0800

    Update diy-part1.sh

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -22,5 +22,5 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 
 # 添加插件源码
 #sed -i '$a src-git-full kenzo https://github.com/RavenCage/kenzok8-openwrt-packages' feeds.conf.default
-#sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git-full small https://github.com/kenzok8/small' feeds.conf.default
