f226f4aba4d8478797a76c708b86c6decd59fb09
RavenCage <126802578+RavenCage@users.noreply.github.com>
2023-10-02 14:35:07 +0800

    Update diy-part2.sh

--- hanwckf/JDCloud-AX6000/diy-part2.sh
+++ hanwckf/JDCloud-AX6000/diy-part2.sh
@@ -33,4 +33,9 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # rm -rf lua-maxminddb
 # git clone https://github.com/jerrykuku/lua-maxminddb
 
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
