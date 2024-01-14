0db4526dcfcac1b6ed668053dbd4c080ac258fe6
RavenCage <126802578+RavenCage@users.noreply.github.com>
2024-01-14 11:35:20 +0800

    Update Handles.sh

--- hanwckf/CMCC-RAX3000M-EMMC/Handles.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/Handles.sh
@@ -67,15 +67,15 @@ if [ -d *"OpenClash"* ]; then
 	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
 	#CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
 
-	#GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
-	#GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
+	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
+	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
 	#GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
 	#GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
 
 	cd ./OpenClash/luci-app-openclash/root/etc/openclash/
 
-	#curl -sfL -o Country.mmdb $GEO_MMDB
-	#curl -sfL -o GeoSite.dat $GEO_SITE
+	curl -sfL -o Country.mmdb $GEO_MMDB
+	curl -sfL -o GeoSite.dat $GEO_SITE
 	#curl -sfL -o GeoIP.dat $GEO_IP
 	#curl -sfL -o GeoIP.metadb $GEO_META
 
