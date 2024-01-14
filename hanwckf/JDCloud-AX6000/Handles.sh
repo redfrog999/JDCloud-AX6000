7ab79bb24a7cc29f79222b2c73f12e2da12d1449
RavenCage <126802578+RavenCage@users.noreply.github.com>
2024-01-14 13:34:42 +0800

    Update Handles.sh

--- hanwckf/JDCloud-AX6000/Handles.sh
+++ hanwckf/JDCloud-AX6000/Handles.sh
@@ -69,15 +69,15 @@ if [ -d *"OpenClash"* ]; then
 
 	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
 	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
-	#GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
-	#GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
+	GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
+	GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
 
 	cd ./OpenClash/luci-app-openclash/root/etc/openclash/
 
 	curl -sfL -o Country.mmdb $GEO_MMDB
 	curl -sfL -o GeoSite.dat $GEO_SITE
-	#curl -sfL -o GeoIP.dat $GEO_IP
-	#curl -sfL -o GeoIP.metadb $GEO_META
+	curl -sfL -o GeoIP.dat $GEO_IP
+	curl -sfL -o GeoIP.metadb $GEO_META
 
 	mkdir ./core/ && cd ./core/
 
