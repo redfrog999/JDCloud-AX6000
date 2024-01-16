9bead676be1e74160481071c7261a18509e6dfe5
RavenCage <126802578+RavenCage@users.noreply.github.com>
2024-01-16 16:42:48 +0800

    Update diy-part2.sh

--- OpenWrt/Redmi-AX6000/diy-part2.sh
+++ OpenWrt/Redmi-AX6000/diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.10.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
