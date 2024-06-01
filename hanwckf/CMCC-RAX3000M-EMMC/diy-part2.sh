db22d9f52ae869d5398523a1135ca8ffeabc415e
David Tai <redfrog999@users.noreply.github.com>
2024-06-01 19:05:08 +0800

    Update diy-part2.sh

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part2.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -24,5 +24,5 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-git clone https://github.com/RavenCage/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
