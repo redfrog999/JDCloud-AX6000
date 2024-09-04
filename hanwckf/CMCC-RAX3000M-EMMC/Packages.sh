c678c82955ac279addca01a8548c2637cb2230cf
David Tai <redfrog999@users.noreply.github.com>
2024-09-04 11:44:22 +0800

    Update Packages.sh

--- hanwckf/CMCC-RAX3000M-EMMC/Packages.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/Packages.sh
@@ -33,7 +33,7 @@ UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "js"
 UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
 
-UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
+#UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
